d0569c4658789f17a955b15fd3c2d6dd544be508
gem install redis-dump <307440205@qq.com>
2019-12-14 15:14:22 +0800

    垂直布局

--- runmainwinhorizontallayout.py
+++ runmainwinhorizontallayout.py
@@ -1,23 +1,19 @@
 # -*- encoding: utf-8 -*-
 """
 @File    : runmainwinhorizontallayout.py
-@Time    :  2019/12/13 22:44
+@Time    :  2019/12/14 15:10
 @Author  : Tianjin
 @Email   : tianjincn@163.com
 @Software: PyCharm
 """
-
 import sys
-import mainwinverticallayout
-
+import mainwinhorizontallayout
 from PyQt5.QtWidgets import QApplication, QMainWindow
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = QMainWindow()
-    ui = mainwinverticallayout.Ui_MainWindow()
-    # 向窗口添加控件
+    ui = mainwinhorizontallayout.Ui_MainWindow()
     ui.setupUi(mainWindow)
     mainWindow.show()
     sys.exit(app.exec_())
