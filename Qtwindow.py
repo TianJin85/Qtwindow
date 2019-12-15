729f8f92b071d2f8e5ac02eac4ce84781fca94b5
gem install redis-dump <307440205@qq.com>
2019-12-15 20:43:23 +0800

    FormLayout

--- Qtwindow.py
+++ Qtwindow.py
@@ -8,13 +8,13 @@
 
 
 import sys
-import mainwingridlayout
+import mainwinformlayout
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = QMainWindow()
-    ui = mainwingridlayout.Ui_MainWindow()
+    ui = mainwinformlayout.Ui_MainWindow()
     ui.setupUi(mainWindow)
     mainWindow.show()
     sys.exit(app.exec_())
