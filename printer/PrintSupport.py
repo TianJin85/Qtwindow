c9f6e00a7dab0d191f4c071bffeeddcd4ce98d26
gem install redis-dump <307440205@qq.com>
2019-12-28 20:43:59 +0800

    使用打印机

--- printer/PrintSupport.py
+++ printer/PrintSupport.py
@@ -12,6 +12,7 @@ from PyQt5 import QtGui, QtWidgets, QtPrintSupport
 from PyQt5.QtWidgets import *
 
 
+
 class PrintSupport(QWidget):
     def __int__(self):
         super(PrintSupport, self).__int__()
@@ -32,7 +33,6 @@ class PrintSupport(QWidget):
         screen =self.editor.grab()
         painter.drawPixmap(10, 10, screen)
         painter.end()
-
 
 
 if __name__ == '__main__':
