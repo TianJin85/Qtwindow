2a7f82cf93edcbe99f243ab0dc87a5ba819fc55b
gem install redis-dump <307440205@qq.com>
2020-01-01 15:52:26 +0800

    增加、修改、删除树控件节点

---
+++ table_tree/ModifyTree.py
@@ -0,0 +1,96 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : ModifyTree.py
+@Time    :  2020/1/1 14:09
+@Author  : Tianjin
+@Email   : tianjincn@163.com
+@Software: PyCharm
+"""
+'''
+    添加、修改和删除树控件中的节点
+'''
+import sys
+from PyQt5.QtWidgets import *
+
+
+class ModifyTree(QWidget):
+    def __int__(self, parent=None):
+        super(ModifyTree, self).__int__()
+
+    def initUI(self):
+        self.setWindowTitle('TreeWidgets列子')
+
+        operatorLayout = QHBoxLayout()
+        addBtn = QPushButton('添加节点')
+        updateBtn = QPushButton('修改节点')
+        deleteBtn = QPushButton('删除节点')
+
+        operatorLayout.addWidget(addBtn)
+        operatorLayout.addWidget(updateBtn)
+        operatorLayout.addWidget(deleteBtn)
+
+        addBtn.clicked.connect(self.addNode)
+        updateBtn.clicked.connect(self.updateNote)
+        deleteBtn.clicked.connect(self.deleteNode)
+
+        self.tree = QTreeWidget()
+
+        self.tree.setColumnCount(2)
+
+        self.tree.setHeaderLabels(['Key', 'Value'])
+
+        root = QTreeWidgetItem(self.tree)
+        root.setText(0, 'root')
+        root.setText(1, '0')
+
+        child1 = QTreeWidgetItem(root)
+        child1.setText(0, 'child1')
+        child1.setText(1, '1')
+
+        child2 = QTreeWidgetItem(root)
+        child2.setText(0, 'child2')
+        child2.setText(1, '2')
+
+        child3 = QTreeWidgetItem(child2)
+        child3.setText(0, 'child3')
+        child3.setText(1, '3')
+        self.tree.clicked.connect(self.onTreeClicked)
+
+        mainLayout = QVBoxLayout(self)
+        mainLayout.addLayout(operatorLayout)
+        mainLayout.addWidget(self.tree)
+        self.setLayout(mainLayout)
+
+    def onTreeClicked(self, index):
+        item = self.tree.currentItem()
+        print(index.row())
+        print('key=%s,value=%s' % (item.text(0), item.text(1)))
+
+    def addNode(self):
+        print('添加节点')
+        item = self.tree.currentItem()
+        print(item)
+        node = QTreeWidgetItem(item)
+        node.setText(0, '新节点')
+        node.setText(1, '新值')
+
+    def updateNote(self):
+        print('修改节点')
+        item = self.tree.currentItem()
+        item.setText(0, '修改节点')
+        item.setText(1, '值以及被修改')
+
+    def deleteNode(self):
+        print('删除节点')
+        item = self.tree.currentItem()
+        root = self.tree.invisibleRootItem()
+        for item in self.tree.selectedItems():
+            (item.parent() or root).removeChild(item)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = ModifyTree()
+    main.initUI()
+    main.show()
+    sys.exit(app.exec_())
