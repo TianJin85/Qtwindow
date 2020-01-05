d73c2e2fe2a5fbbb0c38759642ac1d81b2f6ca18
gem install redis-dump <307440205@qq.com>
2020-01-05 14:58:34 +0800

    对齐方式

--- layout/HBoxLayoutAlign.py
+++ layout/HBoxLayoutAlign.py
@@ -9,33 +9,33 @@
 from PyQt5.QtCore import Qt
 
 '''
-    设置控件的对齐方式
+    垂直盒布局
 '''
 import sys
 from PyQt5.QtWidgets import *
 
 
-class HBoxLayoutAlign(QWidget):
+class VBoxLayout(QWidget):
     def __int__(self):
-        super(HBoxLayoutAlign, self).__int__()
+        super(VBoxLayout, self).__int__()
 
     def initUI(self):
-        self.setWindowTitle("设置控件的对齐方式")
-        hlayout = QHBoxLayout()
-        hlayout.addWidget(QPushButton('按钮1'), 2, Qt.AlignLeft | Qt.AlignBottom)
-        hlayout.addWidget(QPushButton('按钮2'), 4, Qt.AlignLeft | Qt.AlignTop)
-        hlayout.addWidget(QPushButton('按钮3'), 1, Qt.AlignLeft | Qt.AlignTop)
-        hlayout.addWidget(QPushButton('按钮4'), 1, Qt.AlignLeft | Qt.AlignRight)
-        hlayout.addWidget(QPushButton('按钮5'), 1, Qt.AlignLeft | Qt.AlignBottom)
+        self.setWindowTitle("垂直和布局")
+        layout = QVBoxLayout()
+        layout.addWidget(QPushButton('按钮1'))
+        layout.addWidget(QPushButton('按钮2'))
+        layout.addWidget(QPushButton('按钮3'))
+        layout.addWidget(QPushButton('按钮4'))
+        layout.addWidget(QPushButton('按钮5'))
 
         # 设置按钮之间的间隔
-        hlayout.setSpacing(40)
-        self.setLayout(hlayout)
+        layout.setSpacing(40)
+        self.setLayout(layout)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main = HBoxLayoutAlign()
+    main = VBoxLayout()
     main.initUI()
     main.show()
     sys.exit(app.exec_())
