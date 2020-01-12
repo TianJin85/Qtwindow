a6601debec4566c1b6f8dde68541a23eb90b426f
gem install redis-dump <307440205@qq.com>
2020-01-12 10:45:43 +0800

    可以传递多个参数

--- SignalSlot/CustomSignal.py
+++ SignalSlot/CustomSignal.py
@@ -17,14 +17,22 @@ from PyQt5.QtCore import *
 class MyTypeSignal(QObject):
     # 定义一个信号
     sendmsg = pyqtSignal(object)
+    sendmsg1 = pyqtSignal(str, int, int)
 
     def run(self):
         self.sendmsg.emit('Hello PyQt5')
+
+    def run1(self):
+        self.sendmsg1.emit("hello", 3, 4)
 
 
 class MySlot(QObject):
     def get(self, msg):
         print("信息："+ msg)
+
+    def get1(self, msg, a, b):
+        print(msg)
+        print(a+b)
 
 
 if __name__ == '__main__':
@@ -32,8 +40,9 @@ if __name__ == '__main__':
     slot = MySlot()
 
     send.sendmsg.connect(slot.get)
-
+    send.sendmsg1.connect(slot.get1)
     send.run()
+    send.run1()
     send.sendmsg.disconnect(slot.get)       # 断开连接
     send.run()
 
