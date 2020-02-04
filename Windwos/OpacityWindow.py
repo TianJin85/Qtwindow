8511a6ac904017d3bf96e3e4381a6bdff0b496d7
gem install redis-dump <307440205@qq.com>
2020-02-04 23:06:55 +0800

    创建透明窗口

---
+++ Windwos/OpacityWindow.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : OpacityWindow.py
+@Time    :  2020/2/4 23:03
+@Author  : Tianjin
+@Email   : tianjincn@163.com
+@Software: PyCharm
+"""
+'''
+    创建透明窗口
+'''
+import sys
+from PyQt5.Qt import *
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setWindowTitle('窗口的透明读设置')
+    win.setWindowOpacity(0.5)
+    win.show()
+    sys.exit(app.exec_())
